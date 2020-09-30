7228091bf1b126d55452a30b163cf822c53ee029
Yao Lu <luyaocns@gmail.com>
2020-09-30 16:31:40 +0800

    init commit

--- src/DistributedFlux.jl
+++ src/DistributedFlux.jl
@@ -1,5 +1,53 @@
 module DistributedFlux
 
-# Write your package code here.
+using Flux, MPI
+using TensorBoardLogger, Logging
+using ProgressMeter: next!, Progress
+using Flux.Optimise: Params, @progress, runall, batchmemaybe, StopException, update!
+
+bcast!(xs) = MPI.Bcast!(x, 0, MPI.COMM_WORLD)
+
+function allreduce!(xs)
+    x′ = zero(x)
+    MPI.Allreduce!(x, x′, MPI.SUM, MPI.COMM_WORLD)
+    x .= x′ ./ MPI.Comm_size(MPI.COMM_WORLD)
+end
+
+unwrap(x) = hasproperty(x, :data) ? x.data : x
+
+function Flux.train!(loss, ps, data, opt, gradient = Flux.gradient; cb = () -> (), logger = TBLogger(), verbose = false)
+    foreach(bcast!, ps)
+    ps = Params(ps)
+    cb = runall(cb)
+    l̄ = 0f0
+    ndata = length(data)
+    prog = Progress(ndata)
+    @progress for (n, d) in enumerate(data)
+        try
+            local l
+            gs = gradient(ps) do
+                l = loss(batchmemaybe(d)...)
+            end
+            foreach(allreduce! ∘ unwrap, gs)
+            update!(opt, ps, gs)
+            l̄ = ((n - 1) * l̄ + l) / n
+            if verbose
+                prog.desc = "$n/$ndata "
+                next!(prog, showvalues = [(:loss, l), (:avgloss, l̄)])
+                with_logger(logger) do
+                    @info "train" loss=l avgloss=l̄
+                end
+            end
+            cb()
+        catch ex
+            if ex isa StopException
+                break
+            else
+                rethrow(ex)
+            end
+        end
+    end
+    return l̄
+end
 
 end
