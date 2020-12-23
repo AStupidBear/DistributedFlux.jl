ce02f5c801128812774ba3ebad895761466e434c
Yao Lu <luyaocns@gmail.com>
2020-12-23 15:06:23 +0800

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
-using DistributedFlux
-using Test
+using Test, Random, Statistics
+using Flux, DistributedFlux, MPI
 
 @testset "DistributedFlux.jl" begin
-    # Write your tests here.
+    Random.seed!(1234)
+
+    x = randn(Float32, 100, 16)
+    y = mean(x, dims = 1)
+
+    model = Chain(Dense(100, 100), Dense(100, 1))
+
+    function loss(x, y)
+        ŷ = model(x)
+        Flux.mse(y, ŷ)
+    end
+
+    ps = Flux.params(model)
+    opt = ADAM(1e-3, (0.9, 0.999))
+
+    MPI.Init()
+
+    data = Flux.Data.DataLoader((x, y), batchsize = 4)
+    Flux.train!(loss, ps, data, opt, verbose = true)
+
+    MPI.Finalize()
 end
